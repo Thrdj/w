7a60e33019b925e7453dd1c385fee357fa51acd1
Thrdj <kurusagi10@gmail.com>
2022-08-01 13:47:11 +0700

    first mainpage

---
+++ main.php
@@ -0,0 +1,17 @@
+<?php?>
+<!DOCTYPE HTML>
+<html lang=EN>
+    <header>
+        <meta charset="UTF-8">
+        <meta name="keywords" content="HTML, CSS, JavaScript">
+        <meta name="description" content="Free Web tutorials">
+        <meta name="author" content="KUN">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <base href="https://www.mymemolist.com/" target="_blank">
+        <link rel="stylesheet" href="styles.css">
+    </header>
+    <body>
+        
+    </body>
+    <footer></footer>
+</html>
