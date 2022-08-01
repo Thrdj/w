28f0cb2fd0449c2bf1ff6647c66035ffea0faf3f
Thrdj <kurusagi10@gmail.com>
2022-08-01 15:31:15 +0700

    test

--- main.php
+++ main.php
@@ -9,16 +9,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <base href="https://www.mymemolist.com/" target="_blank">
         <link rel="stylesheet" href="styles.css">
-        <Script>Photomainslide.js</Script>
-        <style>.mySlides {display:none;}</style>
-            <div class="w3-content w3-section" style="max-width:500px">
+    <style>.mySlides {display:none;}</style>
+    <div class="w3-content w3-section" style="max-width:500px">
                 <img class="mySlides" src="img_name1.jpg" style="width:100%">
                 <img class="mySlides" src="img_name2.jpg" style="width:100%">
                 <img class="mySlides" src="img_name3.jpg" style="width:100%">
-            </div>
+    </div>
     </header>
-    <body>
-
-    </body>
-    <footer></footer>
 </html>
---
+++ server/address.php
@@ -0,0 +1,31 @@
+<?php?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title> Responsive Payment getway form design in Hindi</title>
+        <link rel="stylesheet" type="text/css" href="payment.css">
+    </head>
+    <body>
+        <header>
+            <div class="container">
+            <div class="left">
+                <h3>BILLING ADDRESS</h3>
+                    <form>
+                        Full name
+                        <input type="text" name"" placeholder="Enter name">
+                        Email
+                        <input type="text" name"" placeholder="Enter email">
+                        Address
+                        <input type="text" name"" placeholder="Enter address>
+                        City
+                        <input type="text" name"" placeholder="Enter city">
+                    </form>
+            </div>
+            <div class="right">
+            </div>
+            </div>
+        </header>
+    </body>
+</html>
