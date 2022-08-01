33f1e7347c9118928ee094a07fb90ba1ff8da998
Thrdj <kurusagi10@gmail.com>
2022-08-01 10:39:28 +0700

    test php

---
+++ server/login.php
@@ -0,0 +1,15 @@
+<?php ?>
+    <!DOCTYPE HTML>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>LIST</title>
+    </head>
+    <body>
+        <h1>Check</h1>
+        <div class="content">
+            <p>this story in the real life.</p>
+        </div>
+    </body>
+    </html>
