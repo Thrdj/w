613b1f5cd497b9ace498c14c94e39b7044ec4003
Thrdj <kurusagi10@gmail.com>
2022-09-28 11:10:08 +0700

    unzip

--- test.py
+++ test.py
@@ -46,4 +46,11 @@ reader = PyPDF2.PdffileReader(pdf)
 page = reader.getPage(0)
 print(page.extractText())
 
+#Unzip Files using Python
+from zipfile import ZipFile
+with ZipFile('binod.zip','r') as zip_object:
+    zip_object.extractall()
+#list of files that are archived in the ZIP file
+print(zip_object.namelist())
+
 #clcoding.com
